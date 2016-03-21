26f51b981a1590a2b8119682896741fcb3c7d0d8
andresmarinkovic <amarinkovic@immersity.cl>
2016-03-21 10:35:04 -0300

    Adds message to display form send status
    Also:
    * Deletes form data if send was successful

--- assets/js/form-sender.js
+++ assets/js/form-sender.js
@@ -1,28 +1,44 @@
 $(document).ready(function(){
   $('#subscription-form').submit(function(event) {
     event.preventDefault(event);
-    $.ajax({
-      url: 'https://api.airtable.com/v0/appOpQ8GCbqjZ2Rl9/Inscritos',
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader('Authorization', 'Bearer keyUQGqomy6yoWSrP');
-      },
-      type: 'POST',
-      dataType: 'json',
-      contentType: 'application/json',
-      processData: false,
-      data: '{"fields": {' +
-      '"Name": "' + $('#name1').val() + '", ' +
-      '"Comments": "' + $('#comment1').val().replace(/\n/g,"\\n") + '", ' +
-      '"Email": "' + $('#email1').val() + '", ' +
-      '"Amount": ' + $('#amount1').val() + ',' +
-      '"Time": "' + $('#time1').val() + '"' +
-      '}}',
-      dataType: 'json',
-      success: function (data) {
-        console.info(data);
-      }
-    });
+    sendFormData();
   });
 });
 
+function sendFormData() {
+  $.ajax({
+    url: 'https://api.airtable.com/v0/appOpQ8GCbqjZ2Rl9/Inscritos',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('Authorization', 'Bearer keyUQGqomy6yoWSrP');
+    },
+    type: 'POST',
+    dataType: 'json',
+    contentType: 'application/json',
+    processData: false,
+    data: '{"fields": {' +
+    '"Name": "' + $('#name1').val() + '", ' +
+    '"Comments": "' + $('#comment1').val().replace(/\n/g,"\\n") + '", ' +
+    '"Email": "' + $('#email1').val() + '", ' +
+    '"Amount": ' + $('#amount1').val() + ',' +
+    '"Time": "' + $('#time1').val() + '"' +
+    '}}',
+    dataType: 'json',
+    success: function() {
+      alert('Datos enviados exitosamente.');
+      deleteFormData();
+    },
+    error: function(error) {
+      console.log(error);
+      alert('Error al enviar los datos, intente nuevamente.')
+    }
 
+  });
+}
+
+function deleteFormData() {
+  $(':input','#subscription-form')
+    .not(':button, :submit, :reset, :hidden')
+    .val('')
+    .removeAttr('checked')
+    .removeAttr('selected');
+}
