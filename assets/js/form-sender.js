a60130b9deb9866d0ea2646571303dd83c1a3259
andresmarinkovic <amarinkovic@immersity.cl>
2016-03-24 10:24:21 -0300

    Adds input validation to contact form
    The following validations where considered:
    * Name is required
    * Email is required
    * Email must be well formatted
    * Amount can't be negative
    
    Other changes included in this commit:
    * Sets amount value to -1 if it's not present (to avoid error with API
    if value is null)
    * Adds bsFormAlerts library to display invalid input alerts
    * Moves lib js files to subdir named lib

--- assets/js/form-sender.js
+++ assets/js/form-sender.js
@@ -1,11 +1,81 @@
+const DEFAULT_AMOUNT_VALUE = -1;
+var amountValue = DEFAULT_AMOUNT_VALUE;
+
 $(document).ready(function(){
   $('#subscription-form').submit(function(event) {
     event.preventDefault(event);
-    sendFormData();
+    if(inputIsValid()) {
+      setAmountValueIfNull();
+      sendFormData();
+    }
   });
 });
 
+function inputIsValid() {
+  var nameIsValid = checkNameValidityAndDisplayMessage();
+  var emailIsValid = checkEmailValidityAndDisplayMessage();
+  var amountIsValid = checkAmountValidityAndDisplayMessage();
+  return nameIsValid && emailIsValid && amountIsValid;
+}
+
+function checkNameValidityAndDisplayMessage() {
+  var name = $("#name1").val();
+  $(document).trigger("clear-alert-id.name");
+  if (!name) {
+    $(document).trigger("set-alert-id-name", [
+      {
+        message: "Debes ingresar tu nombre.",
+        priority: "error"
+      }
+    ]);
+    return false;
+  }
+  return true;
+}
+
+function checkEmailValidityAndDisplayMessage() {
+  var email = $("#email1").val();
+  $(document).trigger("clear-alert-id.email");
+  if (!email || !emailFormatIsCorrect(email)) {
+    $(document).trigger("set-alert-id-email", [
+      {
+        message: "Debes ingresar tu email y éste debe ser válido.",
+        priority: "error"
+      }
+    ]);
+    return false;
+  }
+  return true;
+}
+
+function emailFormatIsCorrect(email) {
+  var mailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return mailRegex.test(email);
+}
+
+function checkAmountValidityAndDisplayMessage() {
+  var amount = $("#amount1").val();
+  $(document).trigger("clear-alert-id.amount");
+  if (amount && (!$.isNumeric(amount) || amount <= 0)) {
+    $(document).trigger("set-alert-id-amount", [
+      {
+        message: "El monto es opcional, pero si ingresas algo debe ser un número positivo.",
+        priority: "error"
+      }
+    ]);
+    return false;
+  }
+  return true;
+}
+
+function setAmountValueIfNull(){
+  if($('#amount1').val()){
+    amountValue = $('#amount1').val()
+  }
+}
+
 function sendFormData() {
+
   $.ajax({
     url: 'https://api.airtable.com/v0/appOpQ8GCbqjZ2Rl9/Inscritos',
     beforeSend: function(xhr) {
@@ -19,7 +89,7 @@ function sendFormData() {
     '"Name": "' + $('#name1').val() + '", ' +
     '"Comments": "' + $('#comment1').val().replace(/\n/g,"\\n") + '", ' +
     '"Email": "' + $('#email1').val() + '", ' +
-    '"Amount": ' + $('#amount1').val() + ',' +
+    '"Amount": ' + amountValue + ',' +
     '"Time": "' + $('#time1').val() + '"' +
     '}}',
     dataType: 'json',
